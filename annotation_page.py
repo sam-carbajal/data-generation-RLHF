2967f1f17be5a4f109a28c2788fbef1fbff913c4
Samantha <samantha.carbajal@investit-academy.de>
2025-09-01 17:38:42 +0200

    'criteria tab width'

--- annotation_page.py
+++ annotation_page.py
@@ -9,7 +9,7 @@ def AnnotationSection(prompt, criteria):
     st.markdown("Bitte bewerte die Geschichten anhand der Kriterien.")
 
     tab_names = list(criteria.keys())
-    tabs = st.tabs(tab_names, width=800)
+    tabs = st.tabs(tab_names)
 
     rankings, justifs = Ranking(tabs, tab_names)
     annotation = AnnotationFormat(rankings, prompt, justifs)

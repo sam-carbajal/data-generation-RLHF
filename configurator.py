ae99f9aebda21e2e3daeafb7c68feda5cdba469c
Samantha <samantha.carbajal@investit-academy.de>
2025-09-01 18:03:59 +0200

    'criteria tab width'

--- configurator.py
+++ configurator.py
@@ -5,18 +5,18 @@ from annotation_page import AnnotationSection
 from startpage import DefaultSettings, InitializeSession
 
 criteria = {
-    "Inhaltliche_Passung": "Aus kindlicher Perspektive mutmaßlich erkennbarer inhaltlicher Bezug zwischen "
+    "K_Bezug": "Aus kindlicher Perspektive mutmaßlich erkennbarer inhaltlicher Bezug zwischen "
                         "Prompt und Resultat",
-    "Schlüssige_Handlung": "Aus kindlicher Perspektive mutmaßlich schlüssige Handlung",
-    "Stimulation": "Aus kindlicher Perspektive mutmaßlich stimulierende Erzählung "
+    "K_konsistent": "Aus kindlicher Perspektive mutmaßlich schlüssige Handlung",
+    "K_Stimulus": "Aus kindlicher Perspektive mutmaßlich stimulierende Erzählung "
                                 "(z. B. lustig, spannend, immersiv)",
-    "Natürliche_Sprache": "Aus pädagogischer Perspektive natürliche Gestalt der Sprache",
-    "Sprachliche_Fokussierung": "Aus pädagogischer Perspektive sprachlich fokussiert "
+    "P_Idiomacy": "Aus pädagogischer Perspektive natürliche Gestalt der Sprache",
+    "P_Cogn_Appr": "Aus pädagogischer Perspektive sprachlich fokussiert "
                                 "(viele kurze, verständliche Sätze und viel einfaches Vokabular; "
                                 "gezielt einzelne „Stepping Stones“, also herausfordernde Sätze und Vokabeln; "
                                 "nur einfache Sprache oder zu schwere Sprache → niedriger Score, ausgewogene "
                                 "Mischung → hoher Score)",
-    "Pädagogische_Handlung": "Aus pädagogischer Perspektive ansprechende Handlung mit schlüssiger "
+    "P_Ped_Appr": "Aus pädagogischer Perspektive ansprechende Handlung mit schlüssiger "
                                 "Ereignisabfolge, angemessener Konfliktdarstellung, interessanten Wendungen"
 }
 
